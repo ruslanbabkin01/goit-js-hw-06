50327fcea5005013ec087c06789a748331fd7690
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-09-01 11:41:09 +0300

    done 10 task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -7,7 +7,6 @@
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 // Все элементы должены иметь случайный цвет фона в формате HEX.Используй готовую функцию getRandomHexColor для получения цвета.
 // Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -29,26 +28,23 @@ function onInputChange(ev) {
   inputValue = ev.currentTarget.value;
 }
 
-function addDivsOnDocument(firstDiv) {
-  refs.boxes.insertAdjacentHTML('beforeend', firstDiv);
-} 
-
-function createBoxes() {
+function createBoxes(inputValue) {
   let width = 30;
   let height = 30;
-  let firstDiv = `<div width="${width}px" height="${height}px" background-color="${getRandomHexColor()}"></div>`;
+  let firstDiv = `<div style="width: ${width}px; height: ${height}px; background-color: ${getRandomHexColor()}"></div>`;
 
-  for (let i = 0; i < inputValue; i+=1) {
+  for (let i = 1; i < inputValue; i+=1) {
     width += 10;
     height += 10;
 
-    inputValue += `<div width="${width}px" height="${height}px" background-color="${getRandomHexColor()}"></div>`;
+    firstDiv += `<div style="width: ${width}px; height: ${height}px; background-color: ${getRandomHexColor()}"></div>`;
   }
-    addDivsOnDocument(firstDiv)
+    refs.boxes.insertAdjacentHTML('beforeend', firstDiv);
 }
 
 function destroyBoxes() {
   refs.boxes.innerHTML = '';
 }
 
-createBoxes(2)
+createBoxes(6)
+
